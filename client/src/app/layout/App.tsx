75341383b30a9ac600905413d2ddf46e98057d0b
HarryColyer <harrycolyer@hotmail.co.uk>
2026-08-07 11:06:56 +0100

    Removed product state from App and added Outlet component for routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,36 +1,14 @@
-import { useEffect, useState } from "react"
-import type { Product } from "../models/Product"
-import Catalog from "../../features/catalog/Catalog"
 import NavBar from "../../components/ui/NavBar"
+import { Outlet } from "react-router"
 
 function App() {
 
-  const [products, setProducts] = useState<Product[]>([])
 
-  useEffect(() => {
-
-    async function getProducts() {
-      try {
-        const res = await fetch("https://localhost:5001/api/products")
-        if (!res.ok) {
-          throw new Error("failed to fetch products")
-        }
-        const data: Product[] = await res.json()
-        setProducts(data);
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    getProducts();
-  }, [])
 
   return (
     <>
-      <main className="">
-        <NavBar imageSrc="/images/ski-logo.png" imageAlt="Company logo, Man skiing"/>
-        <Catalog products={products} />
-      </main>
+      <NavBar imageSrc="/images/ski-logo.png" imageAlt="Company logo, Man skiing" />
+      <Outlet />
     </>
   )
 }
