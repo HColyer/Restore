48fae6278225b7a40c9735965f92d031754d20fb
HarryColyer <harrycolyer@hotmail.co.uk>
2026-08-07 11:08:19 +0100

    Added: Products state and getProducts service

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,22 @@
 import type { Product } from "../../app/models/Product"
+import { useState, useEffect } from "react"
 import ProductList from "./ProductList"
+import { getProducts } from "../../services/CatalogServices"
 
-type CatalogProps = {
-    products: Product[]
-}
 
-export default function Catalog({ products }: CatalogProps) {
+
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        async function loadProducts() {
+            const data = await getProducts()
+            setProducts(data);
+        }
+
+        loadProducts()
+    }, [])
+
     return (
         <>
             <ProductList products={products} />
