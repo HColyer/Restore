35a288f8a191f707c2938575376c6aba0b081d6f
HarryColyer <harrycolyer@hotmail.co.uk>
2026-08-08 00:25:05 +0100

    Updated: added basket page

--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import App from "../layout/App";
 import Catalog from "../../features/catalog/Catalog";
 import ProductPage from "../../features/catalog/ProductPage";
+import BasketPage from "../../features/basket/BasketPage";
 
 export const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
             {
                 path: "/product/:id",
                 element: <ProductPage />
+            },
+            {
+                path: "/basket",
+                element: <BasketPage />
             }
         ]
     },
