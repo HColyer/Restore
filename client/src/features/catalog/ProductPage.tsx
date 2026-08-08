85d2da455e57a88c0c7c111f1bc4fc6e64c352ba
HarryColyer <harrycolyer@hotmail.co.uk>
2026-08-08 10:09:38 +0100

    Changed: bug found using mt

--- client/src/features/catalog/ProductPage.tsx
+++ client/src/features/catalog/ProductPage.tsx
@@ -1,14 +1,16 @@
+import type { Product } from "../../app/models/Product"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
-import { getProduct } from "../../services/CatalogServices"
-import type { Product } from "../../app/models/Product"
+import useBasket from "../../hooks/useBasketContext"
 import Button from "../../components/ui/Button"
 import LoadingSpinner from "../../components/ui/LoadingSpinner"
+import { getProduct } from "../../services/CatalogServices"
 import { Plus } from "lucide-react"
 
 export default function ProductPage() {
     const { id } = useParams();
     const [product, setProduct] = useState<Product | null>(null)
+    const { addItem } = useBasket();
 
     useEffect(() => {
         async function loadProduct() {
@@ -21,31 +23,27 @@ export default function ProductPage() {
 
     if (!product) {
         return (
-            <div className="min-h-screen flex justify-center mt-40">
+            <div className="min-h-screen flex justify-center pt-40">
                 <LoadingSpinner />
             </div>
         )
     }
 
     return (
-        <main className="min-h-screen flex justify-center items-center">
+        <main className="min-h-screen flex justify-center items-center pt-20">
             <section className="grid grid-cols-[400px_400px] gap-5 items-start">
-                <div className="bg-zinc-100 dark:bg-zinc-900 p-4 w-100 rounded-sm">
-                    <img className="aspect-square" src={product.pictureUrl} alt={product.name} />
-                </div>
-                <div className="bg-zinc-100 dark:bg-zinc-900 p-4 rounded-sm space-y-3">
-                    <h1>{product.name}</h1>
-                    <p>
-                        Brand:<br />
-                        {product.brand}
-                    </p>
-                    <p>
-                        Description:<br />
-                        {product.description}
-                    </p>
-                    <div className="flex p-3 justify-between items-center w-full">
+                <img className="aspect-square overflow-hidden rounded-md border-2 border-orange-300 dark:border-purple-400 shadow-sm" src={product.pictureUrl} alt={product.name} />
+                <div className="space-y-6">
+                    <div className="bg-zinc-100 dark:bg-zinc-900 p-4 rounded-sm space-y-3 shadow-sm">
+                        <h1>{product.name}</h1>
+                        <h2>Product Info</h2>
+                        <p>
+                            {product.description}
+                        </p>
+                    </div>
+                    <div className="bg-zinc-100 dark:bg-zinc-900 flex p-4 justify-between items-center w-full col-start-2 shadow-sm">
                         <p className="pl-2">£{(product.price / 100).toFixed(2)}</p>
-                        <Button>
+                        <Button onClick={() => addItem(product)}>
                             <Plus />
                         </ Button>
                     </div>
