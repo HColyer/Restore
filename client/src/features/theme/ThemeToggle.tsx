19e3f6b99ada2fc273705e2e9acc20b6a010c537
HarryColyer <harrycolyer@hotmail.co.uk>
2026-08-08 10:11:12 +0100

    Changed: import useTheme

--- client/src/features/theme/ThemeToggle.tsx
+++ client/src/features/theme/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import useTheme from "../../hooks/useTheme";
+import useTheme from "../../hooks/useThemeContext";
 import { Sun, Moon } from "lucide-react";
 
 export default function ThemeToggle() {
