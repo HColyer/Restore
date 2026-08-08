901dc1ee2d008b79613b51385eecf63224ecb191
HarryColyer <harrycolyer@hotmail.co.uk>
2026-08-08 10:20:08 +0100

    Added: Base Controller for DRY

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] // localhost/5001/api/products
+    [ApiController]
+    public class BaseApiControler : ControllerBase
+    {
+        
+    }
+
+}
